ffd410ea69e70ba877e43f029049862818275d42
John Paul Jaquith <japesjake@gmail.com>
2025-05-08 10:47:21 -0400

    initial commit

---
+++ monsters.py
@@ -0,0 +1,79 @@
+##<a href="https://www.flaticon.com/free-icons/attack" title="attack icons">Attack icons created by AbtoCreative - Flaticon</a>
+##<a href="https://www.flaticon.com/free-icons/monster" title="monster icons">Monster icons created by Smashicons - Flaticon</a>
+import pygame as pg
+import os
+if True:
+    os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0,50)
+    pg.init()
+    pg.mixer.init()
+    music=pg.mixer.music.load("battle2.wav")
+    pg.mixer.music.play(-1)
+    WIDTH=800
+    HEIGHT=800
+    surface=pg.display.set_mode((WIDTH,HEIGHT))
+    pg.display.set_caption("Monsters")
+class Object:
+    def __init__(self,x,y,sizex,sizey,name):
+        self.x=x
+        self.y=y
+        self.sx=x
+        self.sy=y
+        self.dx=self.sx+50
+        self.sizex=sizex
+        self.sizey=sizey
+        self.img=pg.image.load(name)
+        self.img=pg.transform.scale(self.img,(sizex,sizey))
+    def draw(self):
+        surface.blit(self.img,(self.x,self.y))
+class Game:
+    def __init__(self):
+        self.running=True
+        self.click=False
+class Button(Object):
+    def __init__(self,x,y,sizex,sizey,name):
+        super().__init__(x,y,sizex,sizey,name)
+    def is_moused(self):
+        self.mouse=pg.mouse.get_pos()
+        self.mousex,self.mousey=self.mouse
+        if self.mousex>=self.x and self.mousey>=self.y:
+            if self.mousex<=self.x+self.sizex and self.mousey<=self.y+self.sizex:
+                return True
+        return False
+class Monster(Object):
+    def __init__(self,x,y,sizex,sizey,name,hp=100,atk=10):
+        super().__init__(x,y,sizex,sizey,name)
+        self.hp=hp
+        self.atk=atk
+        self.attacking=False
+        self.reversed=False
+    def attack(self):
+        if self.x<=self.dx and self.reversed==False:
+            self.x+=1
+        if self.x==self.dx:
+            self.reversed=True
+        if self.reversed==True:
+            self.x-=1
+        if self.x==self.sx and self.reversed==True:
+            self.attacking=False
+            self.reversed=False
+if True:
+    game=Game()
+    monster=Monster(200,400,50,50,'monster.png')
+    monster2=Monster(HEIGHT-200,400,50,50,'freddy.png')
+    atk_button=Button(400,600,50,50,'sword.png')
+while game.running:
+    for event in pg.event.get():
+        if event.type==pg.QUIT:
+            game.running=False
+        if event.type==pg.MOUSEBUTTONDOWN:
+            game.click=True
+    monster.draw()
+    monster2.draw()
+    atk_button.draw()
+    if monster.attacking:
+        monster.attack()
+    if atk_button.is_moused() and game.click:
+        monster.attacking=True
+    if game.click:
+        game.click=False
+    pg.display.update()
